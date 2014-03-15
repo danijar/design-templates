9755d6909ab49b88eb57b97f1957c5757eef8c43
Danijar Hafner <mail@danijar.org>
2014-03-15 14:07:41 +0100

    form layout and buttons

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 $(function() {
 	// globals
 	var resize = [];
-
-	// fill table with exaple data
-	var columns = 5;
 	var words = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 	words = words.replace(/[.,]+/g, '').split(' ');
+
+	// populate header
+	for (var i = 0; i < 3; ++i)
+		$('header').append('<a href="" class="button">' + _.sample(words) + '</a>');
+
+	// populate list
+	var columns = 5;
 	function cells(tag, columns, amount) {
 		var cells = '<tr>';
 		for (var j = 0; j < columns; ++j)
@@ -15,6 +19,15 @@ $(function() {
 	$('.list thead').append(cells('th', columns, 2));
 	for (var i = 0; i < 50; ++i)
 		$('.list tbody').append(cells('td', columns, 5));
+
+	// populate form
+	$('.form').each(function() {
+		for (var i = 0; i < 5; ++i) {
+			var label = _.sample(words, 2).join(' ');
+			var value = _.sample(words, 10).join(' ');
+			$(this).append('<label>' + label + '<br><input type="text" value="' + value + '"></label>');
+		}
+	});
 
 	// fix table header
 	$('.list thead').css('position', 'fixed');
@@ -30,6 +43,36 @@ $(function() {
 			if (index > columns.length) return;
 			$(this).width(columns[index]);
 		});
+	});
+
+	// adjust text fields to content
+	$('.form input[type=text]').each(function() {
+		var element = $(this);
+		// add element to measure pixel length of text
+		var id = btoa(Math.floor(Math.random() * Math.pow(2, 64)));
+		var tag = $('<span id="' + id + '">' + element.val() + '</span>').css({
+			'display': 'none',
+			'font-family': element.css('font-family'),
+			'font-size': element.css('font-size'),
+		}).appendTo('body');
+		// adjust element width on keydown
+		function update() {
+			// give browser time to add current letter
+			setTimeout(function() {
+				// prevent whitespace from being collapsed
+				tag.html(element.val().replace(/ /g, '&nbsp'));
+				// clamp length and prevent text from scrolling
+				var max = element.parent().width();
+				var size = Math.max(100, Math.min(max, tag.width() + 10));
+				if (size < max)
+					element.scrollLeft(0);
+				// apply width to element
+				element.width(size);
+			}, 0);
+		};
+		update();
+		element.keydown(update);
+		resize.push(update);
 	});
 
 	// on resize
